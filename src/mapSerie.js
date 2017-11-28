7014cb716de47424cea45140d32af5657c4bf34f
jo <jo@surikat.pro>
2017-11-28 03:30:37 +0100

    fix mapSerie

--- src/mapSerie.js
+++ src/mapSerie.js
@@ -1,14 +1,13 @@
-export default function mapSerie(funcs, callback){
-	return funcs.reduce(
-		(promise, func) =>
-		promise.then(result => {
-			let promise;
-			func = callback(func);
-			if(!(promise instanceof Promise)){
-				promise = Promise.resolve(promise);
-			}
-			return promise.then(Array.prototype.concat.bind(result))
-		}),
+export default function mapSerie(arr, callback){
+	return arr.reduce(
+		(promise, item) =>
+			promise.then(result => {
+				let promise = callback(item);
+				if(!(promise instanceof Promise)){
+					promise = Promise.resolve(promise);
+				}
+				return promise.then(Array.prototype.concat.bind(result))
+			}),
 		Promise.resolve([])
 	);
 }
