12c47c27a994454c9ea25e52188147b90bd5f3f4
jo <jo@surikat.pro>
2017-12-03 01:37:52 +0100

    test asyncResolve

--- test/abstract/asyncResolve.js
+++ test/abstract/asyncResolve.js
@@ -2,17 +2,53 @@ export default ({di, expect})=>{
 	
 	return function(){
 		
-		/*
-		class A{}
+		function A(b, c){
+			this.b = b;
+			this.c = c;
+		}
 		
-		di.addRule('A',{
-			classDef: A,
+		async function B(){
+			return 'b';
+		}
+		async function C(){
+			return 'c';
+		}
+		
+		di.addRules({
+			'A': {
+				classDef: A,
+				params: ['B','C'],
+			},
+			'B': {
+				classDef: B,
+				asyncResolve: false, //default
+			},
+			'C': {
+				classDef: C,
+				asyncResolve: true,
+			},
 		});
 		
-		it('',function(){
+		describe('simple async',async function(){
+			
+			const waitA = di.get('A');
+			
+			it('waitA sould be promise',function(){
+				expect(waitA).instanceof(Promise);
+			});
+			
+			const a = await waitA;
+			
+			it('a.b sould be promise',function(){
+				expect(a.b).instanceof(Promise);
+			});
+			it('a.c should be resolved value of promise',function(){
+				expect(a.c).not.instanceof(Promise);
+				expect(a.c).equal('c');
+			});
 			
 		});
-		*/
+		
 		
 	};
 	
