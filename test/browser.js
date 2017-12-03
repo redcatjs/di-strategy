bd21c4857af04802727a860435d367d57f5926fa
jo <jo@surikat.pro>
2017-12-03 19:55:12 +0100

    refacto + add browser tests

--- test/browser.js
+++ test/browser.js
@@ -1,6 +1,39 @@
 import loadTestFactory from './utils/loadTestFactory'
+import container from '../src/webpack'
 
-const loadTest = loadTestFactory('browser');
+const requires = {};
+
+const loadTest = loadTestFactory(container,(name)=>{
+	const path = name.split('/');
+	const root = path.shift();
+	switch(root){
+		case 'rules':
+			return require('./rules/'+path).default;
+		break;
+	}
+});
+
+describe('rules',function(){		
+	
+	
+	loadTest('rules/classDef');
+	loadTest('rules/instanceOf');
+	loadTest('rules/shared');
+	loadTest('rules/params');
+	loadTest('rules/singleton');
+	loadTest('rules/substitutions');
+	loadTest('rules/sharedInTree');
+	loadTest('rules/calls');
+	loadTest('rules/lazyCalls');
+	loadTest('rules/inheritInstanceOf');
+	loadTest('rules/inheritPrototype-decorator');
+	loadTest('rules/inheritMixins');
+	loadTest('rules/asyncResolve');
+	loadTest('rules/asyncCallsSeries');
+	loadTest('rules/asyncCallsParamsSerie');
+
+});
+
 
 describe('autoloadDirs',function(){
 
--- test/node.js
+++ test/node.js
@@ -1,8 +1,31 @@
-import './abstract'
-
 import loadTestFactory from './utils/loadTestFactory'
+import container from '../src/node'
 
-const loadTest = loadTestFactory('node');
+const loadTest = loadTestFactory(container,(name)=>{
+	return require('./'+name).default;
+});
+
+describe('rules',function(){		
+	
+	
+	loadTest('rules/classDef');
+	loadTest('rules/instanceOf');
+	loadTest('rules/shared');
+	loadTest('rules/params');
+	loadTest('rules/singleton');
+	loadTest('rules/substitutions');
+	loadTest('rules/sharedInTree');
+	loadTest('rules/calls');
+	loadTest('rules/lazyCalls');
+	loadTest('rules/inheritInstanceOf');
+	loadTest('rules/inheritPrototype-decorator');
+	loadTest('rules/inheritMixins');
+	loadTest('rules/asyncResolve');
+	loadTest('rules/asyncCallsSeries');
+	loadTest('rules/asyncCallsParamsSerie');
+
+});
+
 
 describe('autoloadDirs',function(){
 
