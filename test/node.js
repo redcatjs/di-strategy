159bbeb94fc23aad0ea8f5aebe4f3a89e02dbe6c
jo <jo@surikat.pro>
2017-12-04 01:53:03 +0100

    test node autoloadDirs

--- test/node.js
+++ test/node.js
@@ -1,3 +1,6 @@
+import path from 'path'
+import { expect } from 'chai'
+
 import loadTestFactory from './utils/loadTestFactory'
 import container from '../src/node'
 
@@ -26,7 +29,42 @@ describe('rules',function(){
 
 });
 
-
 describe('autoloadDirs',function(){
-
+	
+	const di = container({
+		rules:{
+			'app/A': {
+				
+			},
+			'app/B': {
+				
+			},
+			'app/B/C': {
+				
+			},
+		},
+		
+		autoloadDirs: {
+			'app' : container.requireContext('./autoload', true, /\.js$/),
+		},
+		
+	});
+	
+	
+	it('sould be instance of A',function(){
+		const A = di.get('app/A');
+		expect(A).instanceof( require('./autoload/A').default );
+	});
+	
+	it('sould be instance of B',function(){
+		const B = di.get('app/B');
+		expect(B).instanceof( require('./autoload/B').default );
+	});
+	
+	it('sould be instance of C',function(){
+		const C = di.get('app/B/C');
+		expect(C).instanceof( require('./autoload/B/C').default );
+	});
+	
+	
 });
