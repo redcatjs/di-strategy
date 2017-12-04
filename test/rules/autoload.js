c63740210c52e9e132f7c1d1445962a97407bb1d
jo <jo@surikat.pro>
2017-12-04 14:54:11 +0100

    test autoloadPathResolver

--- test/rules/autoload.js
+++ test/rules/autoload.js
@@ -4,16 +4,27 @@ export default ({di, expect})=>{
 	
 	return function(){
 		
-		//di.setAppRoot('app');
+		di.config('autoloadPathResolver',{
+			'app': path.resolve(__dirname, '../autoload'),
+		});
 		
-		di.addRule('app/A',{
-			autoload: true,
-			path: path.resolve(__dirname, '../autoload/A'),
+		di.addRules({
+			'app/A':{
+				autoload: true,
+			},
+			'C':{
+				autoload: true,
+				path: path.resolve(__dirname, '../autoload/C'),
+			},
 		});
 		
 		it('should return an instance of A',function(){
 			const instance = di.get('app/A');
 			expect(instance).instanceof( require('../autoload/A').default );
+		});
+		it('should return an instance of C',function(){
+			const instance = di.get('C');
+			expect(instance).instanceof( require('../autoload/C').default );
 		});
 		
 	};
